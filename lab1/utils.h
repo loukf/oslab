0a54290f68285a6105ea4e19319be6af49bf6486
Loukas <louk.frag@gmail.com>
2026-05-01 17:43:35 +0300

    fdr->fd

--- lab1/utils.h
+++ lab1/utils.h
@@ -1,7 +1,7 @@
 #pragma once
 
-ssize_t safe_write(const int fdr, const void *buff, const size_t len);
+ssize_t safe_write(const int fd, const void *buff, const size_t len);
 
-ssize_t safe_read(const int fdr, const void *buff, const size_t len);
+ssize_t safe_read(const int fd, const void *buff, const size_t len);
 
-ssize_t safe_pread(const int fdr, const void *buff, const size_t len, off_t offset);
+ssize_t safe_pread(const int fd, const void *buff, const size_t len, off_t offset);
